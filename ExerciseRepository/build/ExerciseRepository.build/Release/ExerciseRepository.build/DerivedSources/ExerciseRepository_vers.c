0d52ac87ed92bfb2e2790cae0c648a0953147ddb
Boyce Estes <estes.boyce@gmail.com>
2022-05-24 02:55:53 -0400

    misc files and settings

---
+++ ExerciseRepository/build/ExerciseRepository.build/Release/ExerciseRepository.build/DerivedSources/ExerciseRepository_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char ExerciseRepositoryVersionString[];
+ extern const double ExerciseRepositoryVersionNumber;
+
+ const unsigned char ExerciseRepositoryVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:ExerciseRepository  PROJECT:ExerciseRepository-1" "\n";
+ const double ExerciseRepositoryVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ ExerciseRepository/build/ExerciseRepository.build/Release/ExerciseRepository.build/Objects-normal/x86_64/ExerciseRepository_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/boyceestes/Development/ios/personal/exercise-repository/ExerciseRepository/build/ExerciseRepository.build/Release/ExerciseRepository.build/DerivedSources/ExerciseRepository_vers.c
