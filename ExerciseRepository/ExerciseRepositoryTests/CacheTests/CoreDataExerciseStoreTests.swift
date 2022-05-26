06066ad55a0fca93cc1d813f4d8bba7c9fb68a05
Boyce Estes <estes.boyce@gmail.com>
2022-05-26 00:42:21 -0400

    `CoreDataExerciseStore` runs operations serially

--- ExerciseRepository/ExerciseRepositoryTests/CacheTests/CoreDataExerciseStoreTests.swift
+++ ExerciseRepository/ExerciseRepositoryTests/CacheTests/CoreDataExerciseStoreTests.swift
@@ -76,7 +76,6 @@ class CoreDataExerciseStoreTests: XCTestCase {
 
         let deletionError = delete(exercise, from: sut)
         XCTAssertEqual(deletionError as NSError?, CoreDataExerciseStore.Error.recordNotFound(exercise) as NSError?)
-
     }
     
     
@@ -106,6 +105,37 @@ class CoreDataExerciseStoreTests: XCTestCase {
         XCTAssertEqual(deletionError as NSError?, CoreDataExerciseStore.Error.recordNotFound(exercise) as NSError?)
         
         expect(sut: sut, toRetrieve: .success([exercise]))
+    }
+    
+    
+    func test_coreDataExerciseStore_serialOperations_runInOrder() {
+        
+        let sut = makeSut()
+        let exercise = makeUniqueExerciseTuple().local
+        
+        var completedOperationsInOrder = [XCTestExpectation]()
+
+        let op1 = expectation(description: "Operation 1")
+        sut.insert(exercise: exercise) { _ in
+            completedOperationsInOrder.append(op1)
+            op1.fulfill()
+        }
+        
+        let op2 = expectation(description: "Operation 2")
+        sut.delete(exercise: exercise) { _ in
+            completedOperationsInOrder.append(op2)
+            op2.fulfill()
+        }
+        
+        let op3 = expectation(description: "Operation 3")
+        sut.insert(exercise: makeUniqueExerciseTuple().local) { _ in
+            completedOperationsInOrder.append(op3)
+            op3.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5.0)
+        
+        XCTAssertEqual(completedOperationsInOrder, [op1, op2, op3], "Expected side-effects to run serially but operations finished in the wrong order")
     }
     
     
