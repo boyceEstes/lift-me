1dc15111560c6bbc8befd76359f36510ae8c2729
Boyce Estes <estes.boyce@gmail.com>
2022-06-22 10:53:08 -0400

    Extract `NSManagedObjectContext` stub that was created to simulate failures saving and fetching from Core Data

--- ExerciseRepository/ExerciseRepositoryTests/CacheTests/CoreDataExerciseStoreTests.swift
+++ ExerciseRepository/ExerciseRepositoryTests/CacheTests/CoreDataExerciseStoreTests.swift
@@ -4,58 +4,6 @@ import ExerciseRepository
 import CoreData
 
 
-extension NSManagedObjectContext {
-    
-    static func alwaysFailingFetchStub() -> Stub {
-        Stub(source: #selector(NSManagedObjectContext.execute(_:)),
-             destination: #selector(Stub.execute(_:)))
-    }
-    
-    static func alwaysFailingSaveStub() -> Stub {
-        Stub(source: #selector(NSManagedObjectContext.save),
-             destination: #selector(Stub.save))
-    }
-    
-    
-    class Stub: NSObject {
-        
-        private let source: Selector
-        private let destination: Selector
-        
-        init(source: Selector, destination: Selector) {
-            self.source = source
-            self.destination = destination
-        }
-        
-        
-        @objc
-        func execute(_: Any) throws -> Any {
-            throw anyNSError()
-        }
-        
-        
-        @objc
-        func save() throws -> Any {
-            throw anyNSError()
-        }
-        
-        
-        func startIntercepting() {
-            method_exchangeImplementations(
-                class_getInstanceMethod(NSManagedObjectContext.self, source)!,
-                class_getInstanceMethod(Stub.self, destination)!
-            )
-        }
-        
-        
-        deinit {
-            method_exchangeImplementations(
-                class_getInstanceMethod(Stub.self, destination)!,
-                class_getInstanceMethod(NSManagedObjectContext.self, source)!
-            )
-        }
-    }
-}
 
 class CoreDataExerciseStoreTests: XCTestCase {
 
