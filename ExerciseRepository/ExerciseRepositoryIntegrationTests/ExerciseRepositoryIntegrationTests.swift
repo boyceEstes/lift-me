838d3171553e195db0f4f47921ec2300256eff1e
Boyce Estes <estes.boyce@gmail.com>
2022-06-22 11:15:10 -0400

    Added `ExerciseRepositoryIntegrationTests` testing target

---
+++ ExerciseRepository/ExerciseRepositoryIntegrationTests/ExerciseRepositoryIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  ExerciseRepositoryIntegrationTests.swift
+//  ExerciseRepositoryIntegrationTests
+//
+//  Created by Boyce Estes on 6/22/22.
+//
+
+import XCTest
+
+
+class ExerciseRepositoryIntegrationTests: XCTestCase {
+
+}
