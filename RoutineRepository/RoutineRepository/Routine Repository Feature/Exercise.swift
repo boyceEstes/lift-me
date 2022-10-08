d0f6a9091769fd11aa70bde703678f71eefdfe10
Boyce Estes <estes.boyce@gmail.com>
2022-10-08 19:52:37 -0400

    Modify Exercise access control to create Exercise instances in other modules

--- RoutineRepository/RoutineRepository/Routine Repository Feature/Exercise.swift
+++ RoutineRepository/RoutineRepository/Routine Repository Feature/Exercise.swift
@@ -8,11 +8,18 @@
 import Foundation
 
 public struct Exercise {
-    
+
     public let id: UUID
     public let creationDate: Date
     
     // relationships
     public let exerciseRecords: [ExerciseRecord]
     public let tags: [Tag]
+    
+    public init(id: UUID, creationDate: Date, exerciseRecords: [ExerciseRecord], tags: [Tag]) {
+        self.id = id
+        self.creationDate = creationDate
+        self.exerciseRecords = exerciseRecords
+        self.tags = tags
+    }
 }
