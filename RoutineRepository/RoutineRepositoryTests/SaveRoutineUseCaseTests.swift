3e120c47ccd4f32298fe72702a7625b6694b83a6
Boyce Estes <estes.boyce@gmail.com>
2022-10-10 21:54:38 -0400

    Extract tracking for memory leaks helper method to separate file

--- RoutineRepository/RoutineRepositoryTests/SaveRoutineUseCaseTests.swift
+++ RoutineRepository/RoutineRepositoryTests/SaveRoutineUseCaseTests.swift
@@ -9,15 +9,6 @@ import XCTest
 import RoutineRepository
 
 
-extension XCTestCase {
-    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
-        }
-    }
-}
-
-
 
 class SaveRoutineUseCaseTests: XCTestCase {
     
