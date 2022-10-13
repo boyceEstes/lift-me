24363d0c11576c3f57d02e28552e38679ba28634
Boyce Estes <estes.boyce@gmail.com>
2022-10-12 23:05:29 -0400

    Create CoreDataRoutineStore with model can be fetched from tests

---
+++ RoutineRepository/RoutineRepositoryTests/CoreDataRoutineStoreTests.swift
@@ -0,0 +1,84 @@
+//
+//  CoreDataRoutineStoreTests.swift
+//  RoutineRepositoryTests
+//
+//  Created by Boyce Estes on 10/11/22.
+//
+
+import XCTest
+import RoutineRepository
+
+
+class CoreDataRoutineStore {
+    
+    private let container: NSPersistentContainer
+    private let context: NSManagedObjectContext
+    
+    public init(storeURL: URL, bundle: Bundle = .main) throws {
+        container = try NSPersistentContainer.load(name: "RoutineStore", url: storeURL, in: bundle)
+        context = container.newBackgroundContext()
+    }
+    
+    // Any and all functions that you want
+}
+
+
+private extension NSPersistentContainer {
+    
+    enum LoadingError: Error {
+        case modelNotFound
+        case failedToLoadPersistentStores(Error)
+    }
+    
+    static func load(name: String, url: URL, in bundle: Bundle) throws -> NSPersistentContainer {
+        
+        guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
+            throw LoadingError.modelNotFound
+        }
+        
+        let container = NSPersistentContainer(name: name, managedObjectModel: model)
+        
+        let description = NSPersistentStoreDescription(url: url)
+        container.persistentStoreDescriptions = [description]
+        
+        var loadingError: Error?
+        container.loadPersistentStores { loadingError = $1 }
+        try loadingError.map { throw LoadingError.failedToLoadPersistentStores($0) }
+        
+        return container
+    }
+}
+
+
+private extension NSManagedObjectModel {
+    
+    static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
+        return bundle
+            .url(forResource: name, withExtension: "momd")
+            .flatMap { url in
+                NSManagedObjectModel(contentsOf: url)
+            }
+    }
+}
+
+
+class CoreDataRoutineStoreTests: XCTestCase {
+    
+    func test_coreDataRoutineStore_creation_deliversNoError() {
+        
+        let _ = makeSUT()
+    }
+    
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> CoreDataRoutineStore {
+
+        let bundle = Bundle(for: CoreDataRoutineStore.self)
+        let storeURL = URL(fileURLWithPath: "/dev/null")
+        let sut = try! CoreDataRoutineStore(storeURL: storeURL, bundle: bundle)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+}
+
