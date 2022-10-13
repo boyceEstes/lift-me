8498f607a34f128f292200cee8a0a8307f2d907a
Boyce Estes <estes.boyce@gmail.com>
2022-10-13 13:35:41 -0400

    Replace NSSet data type for ManagedRoutine's unordered RoutineRecords with Set to get out of the box insert/remove functionality

--- RoutineRepository/RoutineRepository/RoutineRepositoryCache/Core Data/ManagedRoutine.swift
+++ RoutineRepository/RoutineRepository/RoutineRepositoryCache/Core Data/ManagedRoutine.swift
@@ -15,26 +15,8 @@ public class ManagedRoutine: NSManagedObject {
     @NSManaged public var id: UUID
     @NSManaged public var name: String
     @NSManaged public var creationDate: Date
-    @NSManaged public var routineRecords: NSSet
+    @NSManaged public var routineRecords: Set<ManagedRoutineRecord>
 }
-
-
-// MARK: Generated accessors for routineRecords
-extension ManagedRoutine {
-
-    @objc(addRoutineRecordsObject:)
-    @NSManaged public func addToRoutineRecords(_ value: ManagedRoutineRecord)
-
-    @objc(removeRoutineRecordsObject:)
-    @NSManaged public func removeFromRoutineRecords(_ value: ManagedRoutineRecord)
-
-    @objc(addRoutineRecords:)
-    @NSManaged public func addToRoutineRecords(_ values: NSSet)
-
-    @objc(removeRoutineRecords:)
-    @NSManaged public func removeFromRoutineRecords(_ values: NSSet)
-}
-
 
 
 extension ManagedRoutine {
