40aa3d6af3eaa78bb51c1ebf42d0fa092b222dde
Boyce Estes <estes.boyce@gmail.com>
2022-10-18 11:22:54 -0400

    LocalRoutineRepository will deliver no items on empty cache after creation with functional store

--- RoutineRepository/RoutineRepositoryCacheIntegrationTests/RoutineRepositoryCacheIntegrationTests.swift
+++ RoutineRepository/RoutineRepositoryCacheIntegrationTests/RoutineRepositoryCacheIntegrationTests.swift
@@ -6,30 +6,44 @@
 //
 
 import XCTest
+import RoutineRepository
 
 class RoutineRepositoryCacheIntegrationTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    func test_localRoutineRepository_emptyCache_deliversNoItems() {
+        
+        let sut = makeSUT()
+        
+        let exp = expectation(description: "Wait for loadAllRoutines completion")
+        sut.loadAllRoutines { result in
+            switch result {
+            case let .success(routines):
+                XCTAssertEqual(routines, [])
+            default:
+                XCTFail("Expected empty routines array to be returned, got \(result) instead")
+            }
+            exp.fulfill()
         }
+        wait(for: [exp], timeout: 1)
     }
-
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalRoutineRepository {
+        let storeURL = specificTestStoreURL()
+        let bundle = Bundle(for: CoreDataRoutineStore.self)
+        let routineStore = try! CoreDataRoutineStore(storeURL: storeURL, bundle: bundle)
+        let localRoutineRepository = LocalRoutineRepository(routineStore: routineStore)
+        return localRoutineRepository
+    }
+    
+    
+    private func specificTestStoreURL() -> URL {
+        return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
+    }
+    
+    
+    private func cachesDirectory() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
 }
