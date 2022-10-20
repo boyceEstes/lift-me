5dadd16d5889618efa9958737659fae75673e3e7
Boyce Estes <estes.boyce@gmail.com>
2022-10-19 22:21:43 -0400

    Routine List View initialization will not message the routine repository

--- RoutineRepository/LiftMeRoutinesiOSTests/LiftMeRoutinesiOSTests.swift
+++ RoutineRepository/LiftMeRoutinesiOSTests/LiftMeRoutinesiOSTests.swift
@@ -7,7 +7,59 @@
 
 import XCTest
 import LiftMeRoutinesiOS
+import RoutineRepository
+import SwiftUI
+
+struct RoutineListView: View {
+    
+    var body: some View {
+        Text("Hello world")
+    }
+}
+
+/*
+ * - Init of view will request no routines
+ * - Appear will request load once
+ * - Failure to load will display failure message
+ *
+ */
+
 
 class LiftMeRoutinesiOSTests: XCTestCase {
 
+    func test_routineListView_init_doesNotRequestRoutineRepository() {
+        
+        let (_, routineRepository) = makeSUT()
+        
+        XCTAssertTrue(routineRepository.requests.isEmpty)
+    }
+    
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (RoutineListView, RoutineRepositorySpy) {
+        
+        let routineRepository = RoutineRepositorySpy()
+        let sut = RoutineListView()
+        trackForMemoryLeaks(routineRepository)
+        
+        return (sut, routineRepository)
+    }
+}
+
+
+class RoutineRepositorySpy: RoutineRepository {
+    
+    enum ReceivedMessage: Equatable {
+        case save(Routine)
+        case loadAllRoutines
+    }
+    
+    private(set) var requests = [ReceivedMessage]()
+    
+    func save(routine: Routine, completion: @escaping SaveRoutineCompletion) {
+        requests.append(.save(routine))
+    }
+    
+    func loadAllRoutines(completion: @escaping LoadAllRoutinesCompletion) {
+        requests.append(.loadAllRoutines)
+    }
 }
