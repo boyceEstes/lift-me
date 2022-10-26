43a60eb33678d90db3c480db5a906a0f5a2320f4
Boyce Estes <estes.boyce@gmail.com>
2022-10-25 22:38:04 -0400

    Created LiftMeApp project

---
+++ LiftMeApp/LiftMeApp/LiftMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftMeAppApp.swift
+//  LiftMeApp
+//
+//  Created by Boyce Estes on 10/25/22.
+//
+
+import SwiftUI
+
+@main
+struct LiftMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
