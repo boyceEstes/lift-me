d6b268bb1a4995e75f9272efa4f44b868b5d0e18
Boyce Estes <estes.boyce@gmail.com>
2022-10-30 22:27:47 -0400

    Create LiftMeApp workspace and link the iOS frameworks that we have imported

--- LiftMeApp/LiftMeApp/ContentView.swift
+++ LiftMeApp/LiftMeApp/ContentView.swift
@@ -6,21 +6,43 @@
 //
 
 import SwiftUI
+import CoreData
+import LiftMeRoutinesiOS
+import RoutineRepository
 
 struct ContentView: View {
+    
+    let routineStore: CoreDataRoutineStore
+    let routineRepository: LocalRoutineRepository
+    
+    
+    init() {
+        
+        let localStoreURL = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("routine-store.sqlite")
+        let bundle = Bundle(for: CoreDataRoutineStore.self)
+        self.routineStore = try! CoreDataRoutineStore(storeURL: localStoreURL, bundle: bundle)
+        self.routineRepository = LocalRoutineRepository(routineStore: routineStore)
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        RoutineListView(viewModel: RoutineViewModel(routineRepository: routineRepository))
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    
     static var previews: some View {
         ContentView()
+    }
+}
+
+class RoutineRepositoryPreview: RoutineRepository {
+    
+    func save(routine: Routine, completion: @escaping SaveRoutineCompletion) {
+        completion(nil)
+    }
+    
+    func loadAllRoutines(completion: @escaping LoadAllRoutinesCompletion) {
+        completion(.success([]))
     }
 }
