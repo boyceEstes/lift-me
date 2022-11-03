108b5e53e447d72b453acb514c2e218f3f044338
Boyce Estes <estes.boyce@gmail.com>
2022-11-03 09:46:26 -0400

    Extract logic from ContentView and place in lower root, `LiftMeAppApp` where the window is composed. We will do all our view composition here

--- LiftMeApp/LiftMeApp/ContentView.swift
+++ LiftMeApp/LiftMeApp/ContentView.swift
@@ -5,105 +5,33 @@
 //  Created by Boyce Estes on 10/25/22.
 //
 
-import SwiftUI
-import CoreData
-import LiftMeRoutinesiOS
-import RoutineRepository
-
-struct ContentView: View {
-    
-    let routineRepository: RoutineRepository
-    
-    init() {
-        
-        let localStoreURL = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("routine-store.sqlite")
-        let bundle = Bundle(for: CoreDataRoutineStore.self)
-        let routineStore = try! CoreDataRoutineStore(storeURL: localStoreURL, bundle: bundle)
-        self.routineRepository = DispatchQueueMainDecorator(decoratee: LocalRoutineRepository(routineStore: routineStore))
-    }
-
-    var body: some View {
-        RoutineListUIComposer.routineListComposedWith(routineRepository: routineRepository)
-    }
-}
-
-
-public final class RoutineListUIComposer {
-    
-    private init() {}
-    
-    
-    public static func routineListComposedWith(routineRepository: RoutineRepository) -> RoutineListView {
-        
-        let viewModel = RoutineViewModel(routineRepository: routineRepository)
-        return RoutineListView(viewModel: viewModel)
-    }
-}
-
-
-class DispatchQueueMainDecorator<T> {
-    
-    let decoratee: T
-    
-    init(decoratee: T) {
-        self.decoratee = decoratee
-    }
-    
-    func dispatch(_ work: @escaping () -> Void) {
-        guaranteeMainThread {
-            work()
-        }
-    }
-    
-    // TODO: Make this check for main queue instead of main thread
-    private func guaranteeMainThread(_ work: @escaping () -> Void) {
-        
-        if Thread.isMainThread {
-            work()
-        } else {
-            DispatchQueue.main.async(execute: work)
-        }
-    }
-}
-
-
-extension DispatchQueueMainDecorator: RoutineRepository where T == RoutineRepository {
-    
-    func save(routine: Routine, completion: @escaping SaveRoutineCompletion) {
-        
-        decoratee.save(routine: routine) { [weak self] error in
-            self?.dispatch {
-                completion(error)
-            }
-        }
-    }
-    
-    
-    func loadAllRoutines(completion: @escaping LoadAllRoutinesCompletion) {
-        
-        decoratee.loadAllRoutines { [weak self] result in
-            self?.dispatch {
-                completion(result)
-            }
-        }
-    }
-}
-
-
-struct ContentView_Previews: PreviewProvider {
-    
-    static var previews: some View {
-        ContentView()
-    }
-}
-
-class RoutineRepositoryPreview: RoutineRepository {
-    
-    func save(routine: Routine, completion: @escaping SaveRoutineCompletion) {
-        completion(nil)
-    }
-    
-    func loadAllRoutines(completion: @escaping LoadAllRoutinesCompletion) {
-        completion(.success([]))
-    }
-}
+//import SwiftUI
+//import CoreData
+//import LiftMeRoutinesiOS
+//import RoutineRepository
+//
+//struct ContentView: View {
+//
+//    var body: some View {
+//        RoutineListUIComposer.routineListComposedWith(routineRepository: routineRepository)
+//    }
+//}
+//
+//
+//struct ContentView_Previews: PreviewProvider {
+//    
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
+//
+//class RoutineRepositoryPreview: RoutineRepository {
+//    
+//    func save(routine: Routine, completion: @escaping SaveRoutineCompletion) {
+//        completion(nil)
+//    }
+//    
+//    func loadAllRoutines(completion: @escaping LoadAllRoutinesCompletion) {
+//        completion(.success([]))
+//    }
+//}
--- LiftMeApp/LiftMeApp/LiftMeAppApp.swift
+++ LiftMeApp/LiftMeApp/LiftMeAppApp.swift
@@ -6,12 +6,49 @@
 //
 
 import SwiftUI
+import RoutineRepository
+import CoreData
 
 @main
 struct LiftMeAppApp: App {
+    
+    let routineRepository: RoutineRepository
+    
+    init() {
+        
+        let localStoreURL = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("routine-store.sqlite")
+        let bundle = Bundle(for: CoreDataRoutineStore.self)
+        let routineStore = try! CoreDataRoutineStore(storeURL: localStoreURL, bundle: bundle)
+        self.routineRepository = DispatchQueueMainDecorator(decoratee: LocalRoutineRepository(routineStore: routineStore))
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RoutineListUIComposer.routineListComposedWith(routineRepository: routineRepository)
         }
     }
 }
+
+
+extension DispatchQueueMainDecorator: RoutineRepository where T == RoutineRepository {
+    
+    func save(routine: Routine, completion: @escaping SaveRoutineCompletion) {
+        
+        decoratee.save(routine: routine) { [weak self] error in
+            self?.dispatch {
+                completion(error)
+            }
+        }
+    }
+    
+    
+    func loadAllRoutines(completion: @escaping LoadAllRoutinesCompletion) {
+        
+        decoratee.loadAllRoutines { [weak self] result in
+            self?.dispatch {
+                completion(result)
+            }
+        }
+    }
+}
+
