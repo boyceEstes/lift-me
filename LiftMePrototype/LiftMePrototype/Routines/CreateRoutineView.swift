df360ae2d663f874df8726db2557b1ad81b61f0e
Boyce Estes <estes.boyce@gmail.com>
2022-11-03 22:51:26 -0400

    Remove hidden back button for create routine view prototype

--- LiftMePrototype/LiftMePrototype/Routines/CreateRoutineView.swift
+++ LiftMePrototype/LiftMePrototype/Routines/CreateRoutineView.swift
@@ -24,7 +24,6 @@ struct CreateRoutineView: View {
                 Text("Placeholder Select ExerciseView")
                 Spacer()
             }
-            .navigationBarBackButtonHidden(true)
             .navigationTitle("Create Routine")
             .navigationBarTitleDisplayMode(.inline)
             
