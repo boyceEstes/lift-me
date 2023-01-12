f7ed5847ca2fb4301a638fecd1e2e05451535c4e
Boyce Estes <estes.boyce@gmail.com>
2023-01-11 22:44:04 -0500

    Create WorkoutView

---
+++ RoutineRepository/LiftMeRoutinesiOS/HomeView/WorkoutView.swift
@@ -0,0 +1,24 @@
+//
+//  WorkoutView.swift
+//  LiftMeRoutinesiOS
+//
+//  Created by Boyce Estes on 1/11/23.
+//
+
+import SwiftUI
+
+public struct WorkoutView: View {
+    
+    public init() {}
+    
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+struct WorkoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        WorkoutView()
+    }
+}
