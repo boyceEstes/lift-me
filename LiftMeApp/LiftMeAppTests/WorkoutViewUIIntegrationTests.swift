470f201d52e27330f3c3e27d1b744ea51037a4d7
Boyce Estes <estes.boyce@gmail.com>
2023-01-11 23:22:08 -0500

    Create WorkoutView test file

---
+++ LiftMeApp/LiftMeAppTests/WorkoutViewUIIntegrationTests.swift
@@ -0,0 +1,21 @@
+//
+//  WorkoutViewUIIntegrationTests.swift
+//  LiftMeAppTests
+//
+//  Created by Boyce Estes on 1/11/23.
+//
+
+import XCTest
+import ViewInspector
+import SwiftUI
+
+final class WorkoutViewUIIntegrationTests: XCTestCase {
+
+    func test_viewInspector_baseLine_succeeds() throws {
+        
+        let expected = "It lives!"
+        let sut = Text(expected)
+        let value = try sut.inspect().text().string()
+        XCTAssertEqual(value, expected)
+    }
+}
