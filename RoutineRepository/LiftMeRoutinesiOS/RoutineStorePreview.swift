89cee72a2cc6f652f2d4c567cd40048ab0bf1607
Boyce Estes <estes.boyce@gmail.com>
2023-01-25 12:03:52 -0500

    Extract RoutineStorePreview and update it to conform to the latest version on RoutineStore

---
+++ RoutineRepository/LiftMeRoutinesiOS/RoutineStorePreview.swift
@@ -0,0 +1,56 @@
+//
+//  RoutineStorePreview.swift
+//  LiftMeRoutinesiOS
+//
+//  Created by Boyce Estes on 1/25/23.
+//
+
+import Foundation
+import RoutineRepository
+
+class RoutineStorePreview: RoutineStore {
+
+    // MARK: - Routines
+    func createUniqueRoutine(_ routine: Routine, completion: @escaping RoutineStore.CreateRoutineCompletion) {
+        completion(nil)
+    }
+    
+    
+    func readAllRoutines(completion: @escaping ReadRoutinesCompletion) {
+        
+        let routine = Routine(
+            id: UUID(),
+            name: "Preview",
+            creationDate: Date(),
+            exercises: [],
+            routineRecords: [])
+        completion(.success([routine]))
+    }
+
+    
+    // MARK: - Routine Records
+    func createRoutineRecord(completion: @escaping CreateRoutineRecordCompletion) {
+        print("")
+    }
+    
+    
+    func updateRoutineRecord(newRoutineRecord: RoutineRepository.RoutineRecord, completion: @escaping UpdateRoutineRecordCompletion) {
+        print("")
+    }
+    
+    
+    func deleteRoutineRecord(routineRecord: RoutineRepository.RoutineRecord, completion: @escaping DeleteRoutineRecordCompletion) {
+        print("")
+    }
+    
+    
+    // MARK: - Exercises
+    func readAllExercises(completion: @escaping ReadExercisesCompletion) {
+        print("")
+    }
+    
+    
+    func createExercise(_ exercise: RoutineRepository.Exercise, completion: @escaping CreateExerciseCompletion) {
+        print("")
+    }
+}
