4965044bd56bc2e00fb79e02905bf8ecf96a9b90
Boyce Estes <estes.boyce@gmail.com>
2023-02-09 23:57:00 -0500

    Made SetRecord properties optional

--- RoutineRepository/RoutineRepository/Routine Repository Feature/SetRecord.swift
+++ RoutineRepository/RoutineRepository/Routine Repository Feature/SetRecord.swift
@@ -13,11 +13,11 @@ public struct SetRecord: Equatable, Hashable {
     public let id: UUID
     public var duration: Int?
     public var repCount: Int?
-    public var weight: Int
-    public var difficulty: Int
+    public var weight: Int?
+    public var difficulty: Int?
     
     
-    public init(id: UUID, duration: Int?, repCount: Int?, weight: Int, difficulty: Int) {
+    public init(id: UUID, duration: Int?, repCount: Int?, weight: Int?, difficulty: Int?) {
         
         self.id = id
         self.duration = duration
